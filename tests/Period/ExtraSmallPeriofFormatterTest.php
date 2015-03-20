681dae29dc15596820feccdae2196afdde60a0ef
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-03-20 10:54:30 +0100

    Added extra test for XS period formatter

--- tests/Period/ExtraSmallPeriofFormatterTest.php
+++ tests/Period/ExtraSmallPeriofFormatterTest.php
@@ -30,4 +30,17 @@ class ExtraSmallPeriofFormatterTest extends \PHPUnit_Framework_TestCase
             $this->formatter->format($period)
         );
     }
+
+    public function testFormatsAPeriodDayWithoutLeadingZero()
+    {
+        $period = new CultureFeed_Cdb_Data_Calendar_Period(
+          '2015-03-01',
+          '2015-03-05'
+        );
+
+        $this->assertEquals(
+          '<span class="cf-date">1</span>/<span class="cf-month">03</span>',
+          $this->formatter->format($period)
+        );
+    }
 }
