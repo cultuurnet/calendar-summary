5962195e032ad834b1ae18edda91c0d52fe3f14b
Kristof Coomans <kristof@coomans.eu>
2015-03-20 11:45:52 +0100

    Finish small period formatter

--- src/Period/SmallPeriodFormatter.php
+++ src/Period/SmallPeriodFormatter.php
@@ -8,18 +8,70 @@
 
 namespace CultuurNet\CalendarSummary\Period;
 
+use \CultureFeed_Cdb_Data_Calendar_Period;
+use \DateTime;
+use \DateTimeInterface;
 
-class SmallPeriodFormatter implements PeriodFomatterInterface {
+class SmallPeriodFormatter implements PeriodFomatterInterface
+{
+    public function format(CultureFeed_Cdb_Data_Calendar_Period $period)
+    {
+        $startDate = $this->dateFromString($period->getDateFrom());
+        $startDate->setTime(0, 0, 1);
 
-    public function format(
-      \CultureFeed_Cdb_Data_Calendar_Period $period
-    ) {
-        $dateFrom = $period->getDateFrom();
+        $now = new DateTime();
 
-        $dateFromDay = date('DD', strtotime($dateFrom));
-        $dateFromMonth = date('M', strtotime($dateFrom));
+        if ($startDate > $now) {
+            return $this->formatNotStarted($startDate);
+        } else {
+            $endDate = $this->dateFromString($period->getDateTo());
+            return $this->formatStarted($endDate);
+        }
+    }
 
-        $output = '<span class="cf-date">' . $dateFromDay . '</span>/<span class="cf-month">' . $dateFromMonth . '</span>';
+    /**
+     * @param string $dateString
+     * @return DateTime
+     */
+    private function dateFromString($dateString)
+    {
+        return DateTime::createFromFormat('Y-m-d', $dateString);
+    }
+
+    /**
+     * @param DateTimeInterface $endDate
+     * @return string
+     */
+    private function formatStarted(DateTimeInterface $endDate)
+    {
+        return
+            '<span class="to meta">Tot</span> ' .
+            $this->formatDate($endDate);
+    }
+
+    /**
+     * @param DateTimeInterface $startDate
+     * @return string
+     */
+    private function formatNotStarted(DateTimeInterface $startDate)
+    {
+        return
+            '<span class="from meta">Vanaf</span> ' .
+            $this->formatDate($startDate);
+    }
+
+    /**
+     * @param DateTimeInterface $date
+     * @return string
+     */
+    private function formatDate(DateTimeInterface $date)
+    {
+        $dateFromDay = $date->format('j');
+        $dateFromMonth = $date->format('M');
+
+        $output =
+            '<span class="cf-date">' . $dateFromDay . '</span> ' .
+            '<span class="cf-month">' . strtolower($dateFromMonth) . '</span>';
 
         return $output;
     }
