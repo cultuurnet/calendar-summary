4001968ff228e60cc3317fb55f683b0512a5d968
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-03-23 09:57:28 +0100

    Added draft for mediumformatter with IntlDateFormatter class

--- src/Period/MediumPeriodFormatter.php
+++ src/Period/MediumPeriodFormatter.php
@@ -8,12 +8,27 @@
 
 namespace CultuurNet\CalendarSummary\Period;
 
+use IntlDateFormatter;
+
 class MediumPeriodFormatter implements PeriodFomatterInterface
 {
 
     public function format(
         \CultureFeed_Cdb_Data_Calendar_Period $period
     ) {
+        $dateFrom = $period->getDateFrom();
+        $dateFormatter = new IntlDateFormatter(
+            'nl_BE',
+            IntlDateFormatter::NONE,
+            IntlDateFormatter::NONE,
+            date_default_timezone_get(),
+            IntlDateFormatter::GREGORIAN,
+            'j F Y');
+        $intlDateFrom = $dateFormatter->format($dateFrom);
 
+
+        $output = '<span class="cf-date">' . $dateFrom . '</span>';
+
+        return $output;
     }
 }
