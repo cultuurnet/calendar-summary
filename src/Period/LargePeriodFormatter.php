294197a195d9a2ed3e8a4c6c750227757622c242
Jonas Verhaeghe <jonas@ubuntuJonas.cnet.local>
2015-03-27 13:54:08 +0100

    timestamps

--- src/Period/LargePeriodFormatter.php
+++ src/Period/LargePeriodFormatter.php
@@ -14,9 +14,82 @@ use IntlDateFormatter;
 class LargePeriodFormatter implements PeriodFomatterInterface
 {
 
+    /**
+     * Translate the day in Dutch.
+     */
+    protected $mapping_days = array(
+        'monday' => 'maandag',
+        'tuesday' => 'dinsdag',
+        'wednesday' => 'woensdag',
+        'thursday' => 'donderdag',
+        'friday' => 'vrijdag',
+        'saturday' => 'zaterdag',
+        'sunday' => 'zondag',
+    );
+
+    protected $mapping_short_days = array(
+        'monday' => 'Mo',
+        'tuesday' => 'Tu',
+        'wednesday' => 'We',
+        'thursday' => 'Th',
+        'friday' => 'Fr',
+        'saturday' => 'Sa',
+        'sunday' => 'Su',
+    );
+
     public function format(
         \CultureFeed_Cdb_Data_Calendar_Period $period
     ) {
+        $output = $this->generateDates($period->getDateFrom(), $period->getDateTo());
+        $output .= $this->generateWeekscheme($period->getWeekScheme());
+
+        return $output;
+    }
+
+
+    protected function getDutchDay($day)
+    {
+        return $this->mapping_days[$day];
+    }
+
+    protected function getFormattedTime($time)
+    {
+        $formatted_time = substr($time, 0, -3);
+        return ltrim($formatted_time, '0');
+    }
+
+    protected function getEarliestTime($times)
+    {
+        $start_time = null;
+        foreach ($times as $time) {
+            if ($start_time==null || $start_time > $time->getOpenFrom()) {
+                $start_time = $time->getOpenFrom();
+            }
+        }
+        if (is_null($start_time)) {
+            return '';
+        } else {
+            return ' ' . $this->getFormattedTime($start_time);
+        }
+    }
+
+    protected function getLatestTime($times)
+    {
+        $end_time = null;
+        foreach ($times as $time) {
+            if ($end_time==null || $end_time < $time->getOpenTill()) {
+                $end_time = $time->getOpenTill();
+            }
+        }
+        if (is_null($end_time)) {
+            return '';
+        } else {
+            return '-' . $this->getFormattedTime($end_time);
+        }
+    }
+
+    protected function generateDates($date_from, $date_to)
+    {
         $fmt = new IntlDateFormatter(
             'nl_BE',
             IntlDateFormatter::FULL,
@@ -25,29 +98,30 @@ class LargePeriodFormatter implements PeriodFomatterInterface
             IntlDateFormatter::GREGORIAN,
             'd MMMM Y'
         );
-        $dateFromString = $period->getDateFrom();
+        $dateFromString = $date_from;
         $dateFrom = strtotime($dateFromString);
         $intlDateFrom =$fmt->format($dateFrom);
 
-        $dateToString = $period->getDateTo();
+        $dateToString =$date_to;
         $dateTo = strtotime($dateToString);
         $intlDateTo = $fmt->format($dateTo);
 
-        $output = '<p>';
-        $output .= '<time itemprop="startDate" datetime="' . $dateFrom . '">';
-        $output .= '<span class="cf-date">' . $intlDateFrom . '</span>';
-        $output .= '<span class="cf-to cf-meta">tot</span>';
-        $output .= '<time itemprop="endDate" datetime="' . $dateTo . '">';
-        $output .= '<span class="cf-date">' . $intlDateTo . '</span>';
-        $output .= '</p>';
+        $output_dates = '<p>';
+        $output_dates .= '<time itemprop="startDate" datetime="' . date("Y-m-d", $dateFrom) . '">';
+        $output_dates .= '<span class="cf-date">' . $intlDateFrom . '</span></time>';
+        $output_dates .= '<span class="cf-to cf-meta">tot</span>';
+        $output_dates .= '<time itemprop="endDate" datetime="' . date("Y-m-d", $dateTo) . '">';
+        $output_dates .= '<span class="cf-date">' . $intlDateTo . '</span></time>';
+        $output_dates .= '</p>';
+        return $output_dates;
+    }
 
-        $output .= '<p>Openingsuren:</p>';
-        $output .= '<ul class="list-unstyled">';
-
-        $weekscheme = $period->getWeekScheme();
+    protected function generateWeekscheme($weekscheme)
+    {
+        $output_week = '<p>Openingsuren:</p>';
+        $output_week .= '<ul class="list-unstyled">';
 
         $keys = array_keys($weekscheme->getDays());
-
 
         for ($i = 0; $i <= 6; $i++) {
             $one_day = $weekscheme->getDays()[$keys[$i]];
@@ -59,50 +133,62 @@ class LargePeriodFormatter implements PeriodFomatterInterface
                 if (!is_null($previous) &&
                     $previous->getOpenType()==$one_day->getOpenType() &&
                     $previous->getOpeningTimes()==$one_day->getOpeningTimes()) {
-                    if (strpos($output, '- ' . $previous->getDayName() . '</span>' !== false)) {
-                        $output = str_replace(
-                            '- ' . $previous->getDayName() . '</span>',
-                            '- ' . $one_day->getDayName() . '</span>',
-                            $output
+                    $one_day_dutch = $this->getDutchDay($one_day->getDayName());
+                    $previous_dutch =$this->getDutchDay($previous->getDayName());
+                    $one_day_short= $this->mapping_short_days[$one_day->getDayName()];
+                    $previous_short=$this->mapping_short_days[$previous->getDayName()];
+                    if (strpos($output_week, '- ' . $previous_dutch . '</span>' !== false)) {
+                        $output_week = str_replace(
+                            '- ' . $previous_dutch . '</span>',
+                            '- ' . $one_day_dutch . '</span>',
+                            $output_week
+                        );
+                        $output_week = str_replace(
+                            '-' . $previous_short . ' ',
+                            '-' . $one_day_short . '</span>',
+                            $output_week
                         );
                     } else {
-                        $output = str_replace(
-                            $previous->getDayName() . '</span>',
-                            $previous->getDayName() . ' - ' . $one_day->getDayName() . '</span>',
-                            $output
+                        $output_week = str_replace(
+                            $previous_dutch . '</span>',
+                            $previous_dutch . ' - ' .$one_day_dutch . '</span>',
+                            $output_week
+                        );
+                        $output_week = str_replace(
+                            'datetime="' . $previous_short . ' ',
+                            'datetime="' . $previous_short . '-' . $one_day_short . ' ',
+                            $output_week
                         );
                     }
                 } else {
-                    $output .= '<li>';
-                    $output .= '<meta itemprop="openingHours" content="' . 'TEMP' . '">';
-                    $output .= '<span class="cf-days">' . $one_day->getDayName() . '</span>';
+                    $one_day_dutch = $this->getDutchDay($one_day->getDayName());
+                    //$output_week .= '<li>';
+                    $output_week .= '<meta itemprop="openingHours" datetime="'
+                        . $this->mapping_short_days[$one_day->getDayName()]
+                        . $this->getEarliestTime($one_day->getOpeningTimes())
+                        . $this->getLatestTime($one_day->getOpeningTimes()) . '"></meta>';
+                    $output_week .= '<li itemprop="openingHoursSpecification">';
+                    $output_week .= '<span class="cf-days">' . $one_day_dutch . '</span>';
                     if (!is_null($one_day->getOpeningTimes())) {
                         foreach ($one_day->getOpeningTimes() as $opening_time) {
-                            $output .= '<span class="cf-from cf-meta">van</span>';
-                            $output .=  substr($opening_time->getOpenFrom(), 0, -3);
+                            $output_week .= '<span itemprop="opens" content="'
+                                . $this->getFormattedTime($opening_time->getOpenFrom())
+                                . '" class="cf-from cf-meta">van</span>';
+                            $output_week .= $this->getFormattedTime($opening_time->getOpenFrom());
                             if (!is_null($opening_time->getOpenTill())) {
-                                $output .= '<span class="cf-to cf-meta">tot</span>' .
-                                    substr($opening_time->getOpenTill(), 0, -3);
+                                $output_week .= '<span itemprop="closes" content="' . $this->getFormattedTime($opening_time->getOpenTill()) . '" class="cf-to cf-meta">tot</span>';
+                                $output_week .= $this->getFormattedTime($opening_time->getOpenTill());
                             }
                         }
                     }
-                    $output .= '</meta>';
-                    $output .= '</li>';
+                    //$output_week .= '</meta>';
+                    $output_week .= '</li>';
+                    //$output_week .= '</li>';
                 }
             }
         }
-        $output .= '</ul>';
+        $output_week .= '</ul>';
 
-        // This generates a localised string of a weekday, still figuring how to get it in the previous code
-        $fmt_day = new IntlDateFormatter(
-            'nl_BE',
-            IntlDateFormatter::FULL,
-            IntlDateFormatter::FULL,
-            date_default_timezone_get(),
-            IntlDateFormatter::GREGORIAN,
-            'eeee'
-        );
-
-        return $output;
+        return $output_week;
     }
 }
--- tests/Period/LargePeriodFormatterTest.php
+++ tests/Period/LargePeriodFormatterTest.php
@@ -25,6 +25,76 @@ class LargePeriodFormatterTest extends \PHPUnit_Framework_TestCase
 
     public function testFormatsAPeriod()
     {
+        $period = new CultureFeed_Cdb_Data_Calendar_Period(
+            '2015-03-20',
+            '2015-03-27'
+        );
+        $weekscheme=new \CultureFeed_Cdb_Data_Calendar_Weekscheme();
 
+        $monday=new \CultureFeed_Cdb_Data_Calendar_SchemeDay(SchemeDay::MONDAY, SchemeDay::SCHEMEDAY_OPEN_TYPE_OPEN);
+        $ot1 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('09:00:00', '13:00:00');
+        $ot2 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('17:00:00', '20:00:00');
+        $monday->addOpeningTime($ot1);
+        $monday->addOpeningTime($ot2);
+
+        $tuesday=new \CultureFeed_Cdb_Data_Calendar_SchemeDay(SchemeDay::TUESDAY, SchemeDay::SCHEMEDAY_OPEN_TYPE_OPEN);
+        $ot3 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('09:00:00', '13:00:00');
+        $ot4= new \CultureFeed_Cdb_Data_Calendar_OpeningTime('17:00:00', '20:00:00');
+        $tuesday->addOpeningTime($ot3);
+        $tuesday->addOpeningTime($ot4);
+
+        $wednesday = new \CultureFeed_Cdb_Data_Calendar_SchemeDay(
+            SchemeDay::WEDNESDAY,
+            SchemeDay::SCHEMEDAY_OPEN_TYPE_OPEN
+        );
+        $ot5 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('09:00:00', '17:00:00');
+        $wednesday->addOpeningTime($ot5);
+
+        $friday = new \CultureFeed_Cdb_Data_Calendar_SchemeDay(SchemeDay::FRIDAY, SchemeDay::SCHEMEDAY_OPEN_TYPE_OPEN);
+        $ot6 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('09:00:00', '13:00:00');
+        $ot7 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('17:00:00', '20:00:00');
+        $friday->addOpeningTime($ot6);
+        $friday->addOpeningTime($ot7);
+
+        $saturday = new \CultureFeed_Cdb_Data_Calendar_SchemeDay(
+            SchemeDay::SATURDAY,
+            SchemeDay::SCHEMEDAY_OPEN_TYPE_OPEN
+        );
+        $ot8 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('09:00:00', '13:00:00');
+        $ot9 = new \CultureFeed_Cdb_Data_Calendar_OpeningTime('17:00:00', '20:00:00');
+        $saturday->addOpeningTime($ot8);
+        $saturday->addOpeningTime($ot9);
+
+        $weekscheme->setDay(SchemeDay::MONDAY, $monday);
+        $weekscheme->setDay(SchemeDay::TUESDAY, $tuesday);
+        $weekscheme->setDay(SchemeDay::WEDNESDAY, $wednesday);
+        $weekscheme->setDay(SchemeDay::FRIDAY, $friday);
+        $weekscheme->setDay(SchemeDay::SATURDAY, $saturday);
+
+
+        $period->setWeekScheme($weekscheme);
+
+        $this->assertEquals(
+            '<p><time itemprop="startDate" datetime="2015-03-20"><span class="cf-date">20 maart 2015</span></time>'
+            . '<span class="cf-to cf-meta">tot</span><time itemprop="endDate" datetime="2015-03-27">'
+            . '<span class="cf-date">27 maart 2015</span></time></p><p>Openingsuren:</p>'
+            . '<ul class="list-unstyled"><meta itemprop="openingHours" datetime="Mo-Tu 9:00-20:00"></meta>'
+            . '<li itemprop="openingHoursSpecification"><span class="cf-days">maandag - dinsdag</span>'
+            . '<span itemprop="opens" content="9:00" class="cf-from cf-meta">van</span>9:00'
+            . '<span itemprop="closes" content="13:00" class="cf-to cf-meta">tot</span>13:00'
+            . '<span itemprop="opens" content="17:00" class="cf-from cf-meta">van</span>17:00'
+            . '<span itemprop="closes" content="20:00" class="cf-to cf-meta">tot</span>20:00</li>'
+            . '<meta itemprop="openingHours" datetime="We 9:00-17:00"></meta><li itemprop="openingHoursSpecification">'
+            . '<span class="cf-days">woensdag</span>'
+            . '<span itemprop="opens" content="9:00" class="cf-from cf-meta">van</span>9:00'
+            . '<span itemprop="closes" content="17:00" class="cf-to cf-meta">tot</span>17:00</li>'
+            . '<meta itemprop="openingHours" datetime="Fr-Sa 9:00-20:00"></meta>'
+            . '<li itemprop="openingHoursSpecification"><span class="cf-days">vrijdag - zaterdag</span>'
+            . '<span itemprop="opens" content="9:00" class="cf-from cf-meta">van</span>9:00'
+            . '<span itemprop="closes" content="13:00" class="cf-to cf-meta">tot</span>13:00'
+            . '<span itemprop="opens" content="17:00" class="cf-from cf-meta">van</span>17:00'
+            . '<span itemprop="closes" content="20:00" class="cf-to cf-meta">tot</span>20:00</li></ul>',
+            $this->formatter->format($period)
+        );
     }
 }
