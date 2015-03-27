8f44b9eda910979ffb92f257068e7434a6149801
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-03-27 21:48:15 +0100

    Added LargeTimestampsPlainTextFormatter

---
+++ src/Timestamps/LargeTimestampsPlainTextFormatter.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 27/03/15
+ * Time: 17:42
+ */
+
+namespace CultuurNet\CalendarSummary\Timestamps;
+
+use IntlDateFormatter;
+
+class LargeTimestampsPlainTextFormatter
+{
+    private $fmt;
+
+    private $fmtWeekDayLong;
+
+    private $fmtWeekDayShort;
+
+    private $fmtTime;
+
+    public function __construct()
+    {
+        $this->fmt = new IntlDateFormatter(
+            'nl_BE',
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            date_default_timezone_get(),
+            IntlDateFormatter::GREGORIAN,
+            'd MMMM Y'
+        );
+
+        $this->fmtWeekDayLong = new IntlDateFormatter(
+            'nl_BE',
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            date_default_timezone_get(),
+            IntlDateFormatter::GREGORIAN,
+            'EEEE'
+        );
+
+        $this->fmtWeekDayShort = new IntlDateFormatter(
+            'nl_BE',
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            date_default_timezone_get(),
+            IntlDateFormatter::GREGORIAN,
+            'EEEEEE'
+        );
+
+        $this->fmtTime = new IntlDateFormatter(
+            'nl_BE',
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            date_default_timezone_get(),
+            IntlDateFormatter::GREGORIAN,
+            'HH:mm'
+        );
+    }
+
+    public function format(
+        \CultureFeed_Cdb_Data_Calendar_TimestampList $timestampList
+    ) {
+        $timestamps_count = iterator_count($timestampList);
+        $timestampList->rewind();
+
+        if ($timestamps_count == 1) {
+            $timestamp = $timestampList->current();
+            return $this->formatSingleTimestamp($timestamp);
+        } else {
+            return $this->formatMultipleTimestamps($timestampList, $timestamps_count);
+        }
+    }
+
+    public function formatSingleTimestamp($timestamp)
+    {
+        $date = $timestamp->getDate();
+        $intlDate = $this->fmt->format(strtotime($date));
+        $intlWeekDay = $this->fmtWeekDayLong->format(strtotime($date));
+        $startTime = $timestamp->getStartTime();
+        $intlStartTime = $this->fmtTime->format(strtotime($startTime));
+        $endTime = $timestamp->getEndTime();
+        $intlEndTime = $this->fmtTime->format(strtotime($endTime));
+
+        $output = $intlWeekDay . ' ' . $intlDate . '\n';
+        if (!empty($endTime)) {
+            $output .= 'van ';
+        } else {
+            $output .= 'om ';
+        }
+        $output .= $intlStartTime;
+        if (!empty($endTime)) {
+            $output .= ' tot ' . $intlEndTime;
+        }
+
+        return $output;
+    }
+
+    public function formatMultipleTimestamps($timestampList, $timestamps_count)
+    {
+        $today = strtotime(date('Y-m-d') . ' 00:00:00');
+        $output = '';
+
+        for ($i = 0; $i < $timestamps_count; $i++) {
+            $timestamp = $timestampList->current();
+            $date = $timestamp->getDate();
+            $intlDate = $this->fmt->format(strtotime($date));
+            $intlWeekDay = $this->fmtWeekDayShort->format(strtotime($date));
+            $startTime = $timestamp->getStartTime();
+            $intlStartTime = $this->fmtTime->format(strtotime($startTime));
+            $endTime = $timestamp->getEndTime();
+            $intlEndTime = $this->fmtTime->format(strtotime($endTime));
+
+            if (strtotime($date) >= $today) {
+                $output .= $intlWeekDay . ' ' . $intlDate . '\n';
+                if (!empty($endTime)) {
+                    $output .= 'van ';
+                } else {
+                    $output .= 'om ';
+                }
+                $output .= $intlStartTime;
+                if (!empty($endTime)) {
+                    $output .= ' tot ' . $intlEndTime;
+                }
+                if ($i != $timestamps_count-1) {
+                    $output .= '\n';
+                }
+            }
+
+            $timestampList->next();
+        }
+
+        return $output;
+    }
+}
---
+++ tests/Timestamps/LargeTimestampsPlainTextFormatterTest.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 27/03/15
+ * Time: 17:46
+ */
+
+namespace CultuurNet\CalendarSummary\Timestamps;
+
+use \CultureFeed_Cdb_Data_Calendar_TimestampList;
+use \CultureFeed_Cdb_Data_Calendar_Timestamp;
+
+class LargeTimestampsPlainTextFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var LargeTimestampsFormatter
+     */
+    protected $formatter;
+
+
+    public function setUp()
+    {
+        $this->formatter = new LargeTimestampsPlainTextFormatter();
+    }
+
+    public function testFormatsATimestampWithStartTime()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00');
+        $timestamp_list->add($timestamp);
+
+        $output = 'zondag 20 september 2020\n';
+        $output .= 'om 09:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsATimestampWithStartTimeWithoutLeadingZero()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-09', '09:00:00');
+        $timestamp_list->add($timestamp);
+
+        $output = 'woensdag 9 september 2020\n';
+        $output .= 'om 09:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsATimestampWithStartTimeAndEndTime()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00', '12:30:00');
+        $timestamp_list->add($timestamp);
+
+        $output = 'zondag 20 september 2020\n';
+        $output .= 'van 09:00 tot 12:30';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestampsWithStartTime()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-21', '10:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-22', '09:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'zo 20 september 2020\n';
+        $output .= 'om 09:00\n';
+        $output .= 'ma 21 september 2020\n';
+        $output .= 'om 10:00\n';
+        $output .= 'di 22 september 2020\n';
+        $output .= 'om 09:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestampsWithStartTimeAndEndTime()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00', '17:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-21', '10:00:00', '18:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-22', '09:00:00', '17:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'zo 20 september 2020\n';
+        $output .= 'van 09:00 tot 17:00\n';
+        $output .= 'ma 21 september 2020\n';
+        $output .= 'van 10:00 tot 18:00\n';
+        $output .= 'di 22 september 2020\n';
+        $output .= 'van 09:00 tot 17:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestampsWithStartTimeOrStartTimeAndEndTime()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-21', '10:00:00', '18:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-22', '09:00:00', '17:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'zo 20 september 2020\n';
+        $output .= 'om 09:00\n';
+        $output .= 'ma 21 september 2020\n';
+        $output .= 'van 10:00 tot 18:00\n';
+        $output .= 'di 22 september 2020\n';
+        $output .= 'van 09:00 tot 17:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestampsWithStartTimeOrStartTimeAndEndTimeWithoutLeadingZero()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-07', '09:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-08', '10:00:00', '18:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-09', '09:00:00', '17:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'ma 7 september 2020\n';
+        $output .= 'om 09:00\n';
+        $output .= 'di 8 september 2020\n';
+        $output .= 'van 10:00 tot 18:00\n';
+        $output .= 'wo 9 september 2020\n';
+        $output .= 'van 09:00 tot 17:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestampsWithPastAndFutureTimestamps()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-03-26', '09:00:00', '17:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-21', '10:00:00', '18:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-22', '09:00:00', '17:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'ma 21 september 2020\n';
+        $output .= 'van 10:00 tot 18:00\n';
+        $output .= 'di 22 september 2020\n';
+        $output .= 'van 09:00 tot 17:00';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+}
