57beab29a602e025e751e358c6ebd98a1f21bbd3
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-03-27 15:30:01 +0100

    Added TExtraSmallTimestampsFormatter with unit tests

--- tests/Timestamps/ExtraSmallTimestampsFormatterTest.php
+++ tests/Timestamps/ExtraSmallTimestampsFormatterTest.php
@@ -8,6 +8,7 @@
 
 namespace CultuurNet\CalendarSummary\Timestamps;
 
+use \CultureFeed_Cdb_Data_Calendar_TimestampList;
 use \CultureFeed_Cdb_Data_Calendar_Timestamp;
 
 class ExtraSmallTimestampsFormatterTest extends \PHPUnit_Framework_TestCase
@@ -20,23 +21,53 @@ class ExtraSmallTimestampsFormatterTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-     //   $this->formatter = new \CultuurNet\CalendarSummary\Timestamps\ExtraSmallTimestampsFormatter();
+        $this->formatter = new ExtraSmallTimestampsFormatter();
     }
 
-    public function testFormatsAsTimestamps()
+    public function testFormatsATimestamp()
     {
-//        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
-//        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-09-20', '09:00:00', '12:30:00');
-//        $timestamp_list->add($timestamp);
-//
-//        $this->assertEquals(
-//            '<span class="cf-date">20</span>/<span class="cf-month">11</span>',
-//            $this->formatter->format($timestamp_list)
-//        );
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-09-20', '09:00:00', '12:30:00');
+        $timestamp_list->add($timestamp);
+
+        $output = '<span class="cf-date">20</span>';
+        $output .= '<span class="cf-month">sep</span>';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
     }
 
-    public function testFormatsAsTimestampsDayWithoutLeadingZero()
+    public function testFormatsATimestampWithoutLeadingZero()
     {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-09-09', '09:00:00', '12:30:00');
+        $timestamp_list->add($timestamp);
 
+        $output = '<span class="cf-date">9</span>';
+        $output .= '<span class="cf-month">sep</span>';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestamps()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-09-20', '09:00:00', '12:30:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-09-21', '09:00:00', '12:30:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2015-09-22', '09:00:00', '12:30:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $this->setExpectedException(
+            '\CultuurNet\CalendarSummary\FormatterException',
+            'xs format not supported for multiple timestamps.'
+        );
+        $this->formatter->format($timestamp_list);
     }
 }
