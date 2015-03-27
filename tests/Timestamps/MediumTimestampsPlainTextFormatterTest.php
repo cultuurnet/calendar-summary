818635a073ba038c713a2b98d25bfcf21809bd52
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-03-27 17:41:31 +0100

    Added MediumTimestampsPlainTextFormatter with unit tests

---
+++ tests/Timestamps/MediumTimestampsPlainTextFormatterTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 27/03/15
+ * Time: 17:17
+ */
+
+namespace CultuurNet\CalendarSummary\Timestamps;
+
+use \CultureFeed_Cdb_Data_Calendar_TimestampList;
+use \CultureFeed_Cdb_Data_Calendar_Timestamp;
+
+class MediumTimestampsPlainTextFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MediumTimestampsFormatter
+     */
+    protected $formatter;
+
+
+    public function setUp()
+    {
+        $this->formatter = new MediumTimestampsPlainTextFormatter();
+    }
+
+    public function testFormatsASingleTimestamp()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00');
+        $timestamp_list->add($timestamp);
+
+        $this->assertEquals(
+            'zondag 20 september 2020',
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsASingleTimestampWithoutLeadingZero()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-09', '09:00:00');
+        $timestamp_list->add($timestamp);
+
+        $this->assertEquals(
+            'woensdag 9 september 2020',
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestamps()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-20', '09:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-21', '09:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-22', '09:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'Van 20 september 2020\n';
+        $output .= 'tot 22 september 2020';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+
+    public function testFormatsMultipleTimestampsWithoutLeadingZero()
+    {
+        $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-07', '09:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-08', '09:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-09-09', '09:00:00');
+        $timestamp_list->add($timestamp1);
+        $timestamp_list->add($timestamp2);
+        $timestamp_list->add($timestamp3);
+
+        $output = 'Van 7 september 2020\n';
+        $output .= 'tot 9 september 2020';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($timestamp_list)
+        );
+    }
+}
