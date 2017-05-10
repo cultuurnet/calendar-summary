bc9332058d71d655adc09b1f658c44cd564ca2d7
Luc Wollants <luc.wollants@apptite.be>
2017-05-10 12:15:24 +0200

    III-2034 Change the test dates to 2020 else they would fail if their date was in the past.

--- tests/Timestamps/LargeTimestampsHTMLFormatterTest.php
+++ tests/Timestamps/LargeTimestampsHTMLFormatterTest.php
@@ -495,19 +495,19 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
     public function testFormatsMultipleIndexedTimestampsThatMakeAPeriod()
     {
         $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
-        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-21', '10:00:01');
-        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-22', '00:00:01');
-        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-23', '00:00:01', '16:00:00');
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-21', '10:00:01');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-22', '00:00:01');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-23', '00:00:01', '16:00:00');
         $timestamp_list->add($timestamp1);
         $timestamp_list->add($timestamp2);
         $timestamp_list->add($timestamp3);
 
         $output = '<ul class="list-unstyled">';
         $output .= '<li>';
-        $output .= '<time itemprop="startDate" datetime="2017-05-21T10:00">';
-        $output .= '<span class="cf-weekday cf-meta">zo</span>';
+        $output .= '<time itemprop="startDate" datetime="2020-05-21T10:00">';
+        $output .= '<span class="cf-weekday cf-meta">do</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">21 mei 2017</span>';
+        $output .= '<span class="cf-date">21 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-from cf-meta">van</span>';
         $output .= ' ';
@@ -516,10 +516,10 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
         $output .= ' ';
         $output .= '<span class="cf-to cf-meta">tot</span>';
         $output .= ' ';
-        $output .= '<time itemprop="endDate" datetime="2017-05-23T16:00">';
-        $output .= '<span class="cf-weekday cf-meta">di</span>';
+        $output .= '<time itemprop="endDate" datetime="2020-05-23T16:00">';
+        $output .= '<span class="cf-weekday cf-meta">za</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">23 mei 2017</span>';
+        $output .= '<span class="cf-date">23 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-time">16:00</span>';
         $output .= '</time>';
@@ -535,12 +535,12 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
     public function testFormatsMultipleIndexedTimestampsAsMultiplePeriods()
     {
         $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
-        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-21', '10:00:01');
-        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-22', '00:00:01');
-        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-23', '00:00:01', '16:00:00');
-        $timestamp4 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-24', '10:00:02');
-        $timestamp5 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-25', '00:00:02');
-        $timestamp6 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-26', '00:00:02', '16:00:00');
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-21', '10:00:01');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-22', '00:00:01');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-23', '00:00:01', '16:00:00');
+        $timestamp4 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-24', '10:00:02');
+        $timestamp5 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-25', '00:00:02');
+        $timestamp6 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-26', '00:00:02', '16:00:00');
         $timestamp_list->add($timestamp1);
         $timestamp_list->add($timestamp2);
         $timestamp_list->add($timestamp3);
@@ -550,10 +550,10 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
 
         $output = '<ul class="list-unstyled">';
         $output .= '<li>';
-        $output .= '<time itemprop="startDate" datetime="2017-05-21T10:00">';
-        $output .= '<span class="cf-weekday cf-meta">zo</span>';
+        $output .= '<time itemprop="startDate" datetime="2020-05-21T10:00">';
+        $output .= '<span class="cf-weekday cf-meta">do</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">21 mei 2017</span>';
+        $output .= '<span class="cf-date">21 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-from cf-meta">van</span>';
         $output .= ' ';
@@ -562,19 +562,19 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
         $output .= ' ';
         $output .= '<span class="cf-to cf-meta">tot</span>';
         $output .= ' ';
-        $output .= '<time itemprop="endDate" datetime="2017-05-23T16:00">';
-        $output .= '<span class="cf-weekday cf-meta">di</span>';
+        $output .= '<time itemprop="endDate" datetime="2020-05-23T16:00">';
+        $output .= '<span class="cf-weekday cf-meta">za</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">23 mei 2017</span>';
+        $output .= '<span class="cf-date">23 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-time">16:00</span>';
         $output .= '</time>';
         $output .= '</li>';
         $output .= '<li>';
-        $output .= '<time itemprop="startDate" datetime="2017-05-24T10:00">';
-        $output .= '<span class="cf-weekday cf-meta">wo</span>';
+        $output .= '<time itemprop="startDate" datetime="2020-05-24T10:00">';
+        $output .= '<span class="cf-weekday cf-meta">zo</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">24 mei 2017</span>';
+        $output .= '<span class="cf-date">24 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-from cf-meta">van</span>';
         $output .= ' ';
@@ -583,10 +583,10 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
         $output .= ' ';
         $output .= '<span class="cf-to cf-meta">tot</span>';
         $output .= ' ';
-        $output .= '<time itemprop="endDate" datetime="2017-05-26T16:00">';
-        $output .= '<span class="cf-weekday cf-meta">vr</span>';
+        $output .= '<time itemprop="endDate" datetime="2020-05-26T16:00">';
+        $output .= '<span class="cf-weekday cf-meta">di</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">26 mei 2017</span>';
+        $output .= '<span class="cf-date">26 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-time">16:00</span>';
         $output .= '</time>';
@@ -602,11 +602,11 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
     public function testFormatsTimestampsMixedWithPeriod()
     {
         $timestamp_list = new CultureFeed_Cdb_Data_Calendar_TimestampList();
-        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-25', '10:00:00', '16:00:00');
-        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-05-25', '20:00:00', '01:00:00');
-        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-06-28', '10:00:01');
-        $timestamp4 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-06-29', '00:00:01');
-        $timestamp5 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2017-06-30', '00:00:01', '16:00:00');
+        $timestamp1 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-25', '10:00:00', '16:00:00');
+        $timestamp2 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-05-25', '20:00:00', '01:00:00');
+        $timestamp3 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-06-28', '10:00:01');
+        $timestamp4 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-06-29', '00:00:01');
+        $timestamp5 = new CultureFeed_Cdb_Data_Calendar_Timestamp('2020-06-30', '00:00:01', '16:00:00');
         $timestamp_list->add($timestamp1);
         $timestamp_list->add($timestamp2);
         $timestamp_list->add($timestamp3);
@@ -615,10 +615,10 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
 
         $output = '<ul class="list-unstyled">';
         $output .= '<li>';
-        $output .= '<time itemprop="startDate" datetime="2017-05-25T10:00">';
-        $output .= '<span class="cf-weekday cf-meta">do</span>';
+        $output .= '<time itemprop="startDate" datetime="2020-05-25T10:00">';
+        $output .= '<span class="cf-weekday cf-meta">ma</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">25 mei 2017</span>';
+        $output .= '<span class="cf-date">25 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-from cf-meta">van</span>';
         $output .= ' ';
@@ -627,15 +627,15 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
         $output .= ' ';
         $output .= '<span class="cf-to cf-meta">tot</span>';
         $output .= ' ';
-        $output .= '<time itemprop="endDate" datetime="2017-05-25T16:00">';
+        $output .= '<time itemprop="endDate" datetime="2020-05-25T16:00">';
         $output .= '<span class="cf-time">16:00</span>';
         $output .= '</time>';
         $output .= '</li>';
         $output .= '<li>';
-        $output .= '<time itemprop="startDate" datetime="2017-05-25T20:00">';
-        $output .= '<span class="cf-weekday cf-meta">do</span>';
+        $output .= '<time itemprop="startDate" datetime="2020-05-25T20:00">';
+        $output .= '<span class="cf-weekday cf-meta">ma</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">25 mei 2017</span>';
+        $output .= '<span class="cf-date">25 mei 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-from cf-meta">van</span>';
         $output .= ' ';
@@ -644,15 +644,15 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
         $output .= ' ';
         $output .= '<span class="cf-to cf-meta">tot</span>';
         $output .= ' ';
-        $output .= '<time itemprop="endDate" datetime="2017-05-26T01:00">';
+        $output .= '<time itemprop="endDate" datetime="2020-05-26T01:00">';
         $output .= '<span class="cf-time">01:00</span>';
         $output .= '</time>';
         $output .= '</li>';
         $output .= '<li>';
-        $output .= '<time itemprop="startDate" datetime="2017-06-28T10:00">';
-        $output .= '<span class="cf-weekday cf-meta">wo</span>';
+        $output .= '<time itemprop="startDate" datetime="2020-06-28T10:00">';
+        $output .= '<span class="cf-weekday cf-meta">zo</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">28 juni 2017</span>';
+        $output .= '<span class="cf-date">28 juni 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-from cf-meta">van</span>';
         $output .= ' ';
@@ -661,10 +661,10 @@ class LargeTimestampsHTMLFormatterTest extends \PHPUnit_Framework_TestCase
         $output .= ' ';
         $output .= '<span class="cf-to cf-meta">tot</span>';
         $output .= ' ';
-        $output .= '<time itemprop="endDate" datetime="2017-06-30T16:00">';
-        $output .= '<span class="cf-weekday cf-meta">vr</span>';
+        $output .= '<time itemprop="endDate" datetime="2020-06-30T16:00">';
+        $output .= '<span class="cf-weekday cf-meta">di</span>';
         $output .= ' ';
-        $output .= '<span class="cf-date">30 juni 2017</span>';
+        $output .= '<span class="cf-date">30 juni 2020</span>';
         $output .= ' ';
         $output .= '<span class="cf-time">16:00</span>';
         $output .= '</time>';
